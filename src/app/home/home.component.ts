4850e14942b7c2326d94d048c74e777660dbf338
5hubham7 <5hubham7adav@gmail.com>
2020-11-02 15:34:34 +0530

    completed in college

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Form } from '@angular/forms'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  onSubmit(form:Form){
+  alert("Successfull login")
+  }
+
+  onCancel(form:Form){
+  alert("Cancelled record")
+  }
+}
+
